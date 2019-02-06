e38f425bac6f186abb290087a444e93914cb07fc
Aaron Thomas <aaron25mt@gmail.com>
2019-02-05 18:11:34 -0600

    Initialize project structure

---
+++ src/test/groovy/com/mudndcapstone/server/ServerApplicationTests.groovy
@@ -0,0 +1,17 @@
+package com.mudndcapstone.server
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
