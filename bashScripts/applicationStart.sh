3247f4a084b9fc6335a382303977f69d22b35c4c
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-13 11:32:21 -0600

    edited applicationStart.sh

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,3 +1,3 @@
 echo "running applicationStart script"
-nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
