c9a00cf87481a9d6bf4a7a4453d7bcadacf9f01a
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-13 03:05:00 -0600

    edited applicationStart.sh so spring should now run without halting the deployment

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,2 +1,3 @@
 echo "running applicationStart script"
-java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar
+nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar &
+echo "completed applicationStart script"
