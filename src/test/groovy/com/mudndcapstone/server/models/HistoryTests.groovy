2cd1e785f3df5f75e0d11a910eee9167eab2f55f
Ryan Lokugamage <Ryan@mwc-090139.dhcp.missouri.edu>
2019-02-18 12:31:06 -0600

    added the rest of the model test files

---
+++ src/test/groovy/com/mudndcapstone/server/models/HistoryTests.groovy
@@ -0,0 +1,11 @@
+package com.mudndcapstone.server.models
+
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class HistoryTests {
+
+}
