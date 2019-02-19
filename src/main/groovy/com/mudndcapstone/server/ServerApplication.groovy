7cf008b52eff7ea9046860cb76ba53df46316d7b
Aaron Thomas <aaron25mt@gmail.com>
2019-02-18 22:25:02 -0600

    Add neo4j annotation to application file

--- src/main/groovy/com/mudndcapstone/server/ServerApplication.groovy
+++ src/main/groovy/com/mudndcapstone/server/ServerApplication.groovy
@@ -2,10 +2,11 @@ package com.mudndcapstone.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@EnableNeo4jRepositories
 @SpringBootApplication
 class ServerApplication {
 
