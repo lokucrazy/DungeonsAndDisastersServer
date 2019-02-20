66b71d6ff1db8413dbff25256dda387a5ac1669a
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-20 12:36:45 -0600

    removed unused import from UserServiceImplTests, also testing github slack integration

--- src/test/groovy/com/mudndcapstone/server/services/impl/UserServiceImplTests.groovy
+++ src/test/groovy/com/mudndcapstone/server/services/impl/UserServiceImplTests.groovy
@@ -11,7 +11,6 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner)
