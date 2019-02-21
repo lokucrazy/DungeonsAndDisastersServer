2ac2f44c4cfd52c0c1055e5da91bb808b2990825
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 11:42:59 -0600

    changed dev properties port to 8080, used iptables for redirects, hopefully this works

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-nohup ls 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+source /etc/environment
+nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
--- bashScripts/executeJar.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source /etc/environment
-java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar
