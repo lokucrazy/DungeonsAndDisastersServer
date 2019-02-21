4d27e2896cd845b91eb34cec16f4a8e5b97f9000
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 10:49:10 -0600

    having nohup source the environment file, should work

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-source /etc/environment
-nohup echo $SPRING_PROFILE && java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+nohup source /etc/environment && java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
