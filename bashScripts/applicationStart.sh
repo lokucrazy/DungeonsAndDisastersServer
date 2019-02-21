890f36a60da3da6634bc9b69ef760b96a58c508f
Ryan Lokugamage <Ryan@Ryans-MBP.hitronhub.home>
2019-02-21 11:56:23 -0600

    adding direct command line flag to set profile

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
 source /etc/environment
-nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+nohup java -Dspring.profiles.active=dev -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
