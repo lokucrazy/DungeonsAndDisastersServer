a3057bf16be4a8bd285f0749ae6ec64e924b79fd
Ryan Lokugamage <Ryan@mwc-090139.dhcp.missouri.edu>
2019-02-21 13:35:31 -0600

    removed spring.profiles.active flag in java execution, application.properties should pick up the environment variable now

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,4 +1,4 @@
 echo "running applicationStart script"
 source /etc/environment
-nohup java -Dspring.profiles.active=dev -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
