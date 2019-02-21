8242a42924893e401da2c0f7a20121561a415332
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 10:45:33 -0600

    trying to see if nohup is getting the environment

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
 source /etc/environment
-echo $SPRING_PROFILE
-nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+nohup echo $SPRING_PROFILE && java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
