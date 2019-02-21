b8fa5bf1dbaaf3dfa8b23e392376eb6694f2e211
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 10:27:48 -0600

    codedeploy might not be reading env vars set in /etc/environment

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-echo $SPRING_PROFILE
 source /etc/environment
+echo $SPRING_PROFILE
 nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
