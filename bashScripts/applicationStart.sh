53d4c53bae9e372f2de02e040ead8c4266385ef8
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 12:03:58 -0600

    got rid of shebang lines

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 echo "running applicationStart script"
 source /etc/environment
 nohup java -Dspring.profiles.active=dev -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
--- bashScripts/beforeInstall.sh
+++ bashScripts/beforeInstall.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 source /etc/environment
 pidNumber=$(lsof -t -i :80)
 if [ "$pidNumber" != "" ]
