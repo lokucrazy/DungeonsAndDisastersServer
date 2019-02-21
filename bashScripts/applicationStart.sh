c6f8b522b178f01e1bf1a151a939e099ee47279b
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 11:05:53 -0600

    going to see what happens when it tries to run the jar without nohup

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-nohup source /etc/environment && java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar
 echo "completed applicationStart script"
