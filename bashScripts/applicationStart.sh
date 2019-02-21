e6c7ccee457df0aa9424626183b9d7be7e1f37c1
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 10:19:39 -0600

    checking to see if codedeploy can actually find the environment variable

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-source /etc/enivronment
+echo $SPRING_PROFILE
+source /etc/environment
 nohup java -jar /home/ec2-user/serverJar/server-0.0.1-SNAPSHOT.jar 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
