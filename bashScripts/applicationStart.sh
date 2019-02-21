fd5f19afc41ff04257f2316edfbad0bc6150a386
Ryan Lokugamage <Ryan@Ryans-MacBook-Pro.local>
2019-02-21 11:22:35 -0600

    trying to see where nohup starts at

--- bashScripts/applicationStart.sh
+++ bashScripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "running applicationStart script"
-nohup ./executeJar.sh 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
+nohup ls 1>/home/ec2-user/serverJar/springOut.txt 2>/home/ec2-user/serverJar/springErr.txt &
 echo "completed applicationStart script"
